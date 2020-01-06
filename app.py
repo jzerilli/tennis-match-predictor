f3ff05d27d91cd47440d516f7c141d722ba52545
jzerilli <jzeril01@tufts.edu>
2020-01-06 16:46:23 -0500

    finished inital web app'

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ import sys
 from scraper import getPlayerStats, getHeadToHead 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from flask_bootstrap import Bootstrap
+import pandas as pd
+    
+
 
 app = Flask(__name__) #Initialize the flask App
-Bootstrap(app)
-# model = pickle.load(open('model.pkl', 'rb'))
 
-pred_cols = ['is_clay', 'is_hard' ,'is_grass', 'is_carpet', 'tourney_level', 'best_of', 'rank_diff','age_diff', 'ht_diff', 'hand_diff', 
+with open(f'model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+headers = ['is_clay', 'is_hard' ,'is_grass', 'is_carpet', 'tourney_level', 'best_of', 'rank_diff','age_diff', 'ht_diff', 'hand_diff', 
 '1yr_serve_pct_diff', '1yr_ace_pct_diff', '1yr_bp_save_pct_diff','1yr_serve_pts_won_pct_diff', '1yr_break_pct_diff', '1yr_ret_pts_won_pct_diff',
 'year_win_pct_diff', 'year_surface_win_pct_diff', 'wins_vs_opp_diff', 'wildcard_diff', 'qualifier_diff']
 
@@ -25,32 +28,54 @@ def predict():
     For rendering results on HTML GUI
     '''
 
-    features = request.form.values()
+
 
     p1 = request.form.get('p1_name')
     p2 = request.form.get('p2_name')
 
-    # chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--window-size=1920x1080")
-    # driver=webdriver.Chrome('./chromedriver', options = chrome_options)
-    # surface = request.form.get('Surface')
-    # print(p1, "stats ", getPlayerStats(driver, p1, surface))
+    # tourney_round = request.form.get('Round')
+    level = int(request.form.get('Level'))
+    bestof = int(request.form.get('bestof'))
 
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920x1080")
+    driver=webdriver.Chrome('./chromedriver', options = chrome_options)
+    driver.get("https://www.ultimatetennisstatistics.com/")
+
+    surface = request.form.get('Surface')
+
+    p1_stats = getPlayerStats(driver, p1, surface)
+    p2_stats = getPlayerStats(driver, p2, surface)
+    h2h = getHeadToHead(driver, p1, p2)
+
+    driver.quit()
+
+
+    data = []
+    if surface == "C":
+        data = [1, 0, 0, 0]
+    elif surface == "H":
+        data = [0, 1, 0, 0]
+    else:
+        data = [0, 0, 1, 0]
+
+    data += [level, bestof]
+    data += list(map(lambda x, y: x - y, p1_stats, p2_stats))
+    data += [(h2h[0] - h2h[1])]
+    data += [0, 0]
+
+
+    input_variables = pd.DataFrame([data],
+                                columns=headers, 
+                                dtype=float)
     
-    tourney_round = request.form.get('Round')
-    level = request.form.get('Level')
+    print(model.predict(input_variables))
+    probs = model.predict_proba(input_variables)
+    p1_prob = round(probs[0,1] * 100, 2)
+    p2_prob = round(probs[0,0] * 100, 2)
 
-    # driver.close()
-    # print(str(select))
-
-    # int_features = [int(x) for x in request.form.values()]
-    # final_features = [np.array(int_features)]
-    # prediction = model.predict(final_features)
-
-    # output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Expected winner $ {}'.format(str(p1)))
+    return render_template('index.html', prediction_text=f'Winner Probabilities {p1} : {p1_prob}% {p2} {p2_prob}%')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scraper.py
+++ scraper.py
@@ -5,84 +5,112 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 import time
+from selenium.webdriver.chrome.options import Options
+import bs4
+import requests
 
+# https://www.ultimatetennisstatistics.com/headToHead?name1=Rafael%20Nadal&name2=Novak%20Djokovic
+# https://www.ultimatetennisstatistics.com/playerProfile?playerId=3819&tab=statistics&surface=G&season=-1
 
-def getPlayerPage(driver, player_name):
-	driver.get("https://www.ultimatetennisstatistics.com/")
-	search=driver.find_element_by_id('player')
-	search.send_keys(player_name)
-	time.sleep(1)
-	search=driver.find_element_by_id('ui-id-1')
+# https://www.ultimatetennisstatistics.com/playerProfile?playerId=3819&tab=matches&season=&fromDate=&toDate=&level=&bestOf=&surface=G&indoor=&speed=&round=&result=&tournamentId=&opponent=&countryId=&outcome=played
 
-	items = search.find_elements_by_tag_name("li")
-	items[0].click()
+# def getPlayerPage(driver, player_name):
+# 	search=driver.find_element_by_id('player')
+# 	search.send_keys(player_name)
+
+# 	WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="ui-id-1"]/li[1]'))).click()
 
 def getPlayerStats(driver, player_name, surface):
-	getPlayerPage(driver, player_name)
-	#select statistcs tab
-	statstab=driver.find_element_by_xpath('//*[@id="playerPills"]/li[9]/a').click()
-	statstab=driver.find_element_by_xpath('//*[@id="statisticsPill"]').click()
-	time.sleep(1)
+	url = f'https://www.ultimatetennisstatistics.com/playerProfile?name={player_name}'
+	driver.get(url)
 
-	#set date filter to past year
-	date_range = Select(driver.find_element_by_xpath('//*[@id="statsSeason"]'))
-	date_range.select_by_value('-1')
-	time.sleep(1)
+	html = driver.find_element_by_xpath('//*[@id="profile"]/div').get_attribute('innerHTML')
+	soup = bs4.BeautifulSoup(html, features="lxml")
+
+	tables = soup.findAll("table")
+	bio_table = tables[0]  
+	rank_table = tables[1] 
+
+	age = bio_table.find("th", text="Age").find_next_sibling("td").text
+	age = int(age[:age.find(' ')])
+	height = bio_table.find("th", text="Height").find_next_sibling("td").text
+	height = int(height[:height.find(' ')])
+	hand = bio_table.find("th", text="Plays").find_next_sibling("td").text
+	if hand == 'Left-handed':
+		hand = 1
+	else:
+		hand = 0
+
+	rank = rank_table.find("th", text="Current Rank").find_next_sibling("td").text
+	rank = int(rank[:rank.find(' ')])
+	
+	url = f'https://www.ultimatetennisstatistics.com/playerProfile?name={player_name}&tab=statistics&season=-1'
+	driver.get(url)
+	html = driver.find_element_by_xpath('//*[@id="playerStatsTab"]').get_attribute('innerHTML')
+	soup = bs4.BeautifulSoup(html, features="lxml")
+
+	tables = soup.findAll("table")
+
+	serve_table = tables[0]  
+	return_table = tables[1] 
+	total_table = tables[2] 
+
+	ace_pct = float(serve_table.find("td", text="Ace %").find_next_sibling("th").text[:-1])/100
+	fst_serve_pct = float(serve_table.find("td", text="1st Serve %").find_next_sibling("th").text[:-1])/100
+	serve_pt_pct = float(serve_table.find("td", text="Service Points Won %").find_next_sibling("th").text[:-1])/100
+	bp_save_pct = float(serve_table.find("td", text="Break Points Saved %").find_next_sibling("th").text[:-1])/100
+
+	break_pct = float(return_table.find("td", text="Break Points Won %").find_next_sibling("th").text[:-1])/100
+	return_pt_pct = float(return_table.find("td", text="Return Points Won %").find_next_sibling("th").text[:-1])/100
+
+	win_pct = float(total_table.find("td", text="Matches Won %").find_next_sibling("th").text.strip('\n')[:-1])/100
+
+	url = f'https://www.ultimatetennisstatistics.com/playerProfile?name={player_name}&tab=statistics&season=-1&surface={surface}'
+	driver.get(url)
+
+	html = driver.find_element_by_xpath('//*[@id="playerStatsTab"]').get_attribute('innerHTML')
+	soup = bs4.BeautifulSoup(html, features="lxml")
+
+	tables = soup.findAll("table")
+	total_table = tables[2] 
+
+	# surface_ace_pct = float(serve_table.find("td", text="Ace %").find_next_sibling("th").text[:-1])/100
+	# surface_fst_serve_pct = float(serve_table.find("td", text="1st Serve %").find_next_sibling("th").text[:-1])/100
+	# surface_serve_pt_pct = float(serve_table.find("td", text="Service Points Won %").find_next_sibling("th").text[:-1])/100
+	# surface_bp_save_pct = float(serve_table.find("td", text="Break Points Saved %").find_next_sibling("th").text[:-1])/100
+
+	# surface_break_pct = float(return_table.find("td", text="Break Points Won %").find_next_sibling("th").text[:-1])/100
+	# surface_return_pt_pct = float(return_table.find("td", text="Return Points Won %").find_next_sibling("th").text[:-1])/100
+
+	surface_win_pct = float(total_table.find("td", text="Matches Won %").find_next_sibling("th").text.strip('\n')[:-1])/100
 
 
-	serve_table = driver.find_element_by_xpath('//*[@id="statisticsOverview"]/div/div[1]/table')
-	serve_rows = serve_table.find_elements(By.TAG_NAME, "tr")
+	return (rank, age, height, hand, fst_serve_pct, ace_pct, bp_save_pct, serve_pt_pct, break_pct, return_pt_pct, win_pct, surface_win_pct)
 
-	return_table=driver.find_element_by_xpath('//*[@id="statisticsOverview"]/div/div[2]/table')
-	return_rows = return_table.find_elements(By.TAG_NAME, "tr")
-
-	stats = serve_rows[1].text, serve_rows[3].text, serve_rows[6].text, return_rows[5].text, return_rows[6].text
-
-	#repeat for surface specific data
-	surface_filter = Select(driver.find_element_by_xpath('//*[@id="statsSurface"]'))
-	surface_filter.select_by_value(surface)
-	time.sleep(1)
-
-	surface_serve_table = driver.find_element_by_xpath('//*[@id="statisticsOverview"]/div/div[1]/table')
-	surface_serve_rows = surface_serve_table.find_elements(By.TAG_NAME, "tr")
-
-	surface_return_table=driver.find_element_by_xpath('//*[@id="statisticsOverview"]/div/div[2]/table')
-	surface_return_rows = surface_return_table.find_elements(By.TAG_NAME, "tr")
-
-	surface_stats = surface_serve_rows[1].text, surface_serve_rows[3].text, surface_serve_rows[6].text, surface_return_rows[5].text, surface_return_rows[6].text
-
-	get_raw = lambda a: float(a[a.find('%') + 1: -1])/100
-	return tuple(map(get_raw, stats)), tuple(map(get_raw, surface_stats))
 
 def getHeadToHead(driver, p1, p2):
-	getPlayerPage(driver, p1)
-	search=driver.find_element_by_id('h2hPlayer')
-	search.send_keys(p2)
-	time.sleep(1)
-	search=driver.find_element_by_id('ui-id-2')
-	items = search.find_elements_by_tag_name("li")
-	items[0].click()
-	time.sleep(1)
+	url = f'https://www.ultimatetennisstatistics.com/headToHead?name1={p1}&name2={p2}'
+	driver.get(url)
+	html = driver.page_source
+	soup = bs4.BeautifulSoup(html, features="lxml")
 
-	p1_wins = int(driver.find_element_by_xpath('//*[@id="profiles"]/table/tbody/tr[2]/th[1]').text)
-	p2_wins = int(driver.find_element_by_xpath('//*[@id="profiles"]/table/tbody/tr[2]/th[3]').text)
-
-	return p1_wins, p2_wins
+	table = soup.find("table")
+	p1_wins = int(table.find("th", text="H2H").find_previous_sibling("th").text)
+	p2_wins = int(table.find("th", text="H2H").find_next_sibling("th").text)
+	return (p1_wins, p2_wins)
 
 
-	
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
+# driver=webdriver.Chrome('./chromedriver', options = chrome_options)
 
+# t0 = time.time()
+# print(getPlayerStats(driver, 'Roger Federer', 'C'))
+# print(getPlayerStats(driver, 'Novak Djokovic', 'C'))
+# print(getHeadToHead(driver, 'Roger Federer', 'Novak Djokovic'))
 
-
-driver=webdriver.Chrome('./chromedriver')
-time.sleep(1)
-x = getPlayerStats(driver, 'Roger Federer','C')
-print('fed stats ', x)
-x = getPlayerStats(driver,'Novak Djokovic', 'C')
-print('djoko stats ', x)
-h2h = getHeadToHead(driver, 'Roger Federer', 'Novak Djokovic')
-print('h2h ', h2h)
-
-# driver.close()
+# t1 = time.time()
+# print(t1-t0)
+# driver.quit()
 
 
